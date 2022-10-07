46ce694d5041b35267ffb9231e55b203ed70ab24
Radinal Saragih <rss@radinalsaragih@gmail.com>
2022-10-07 07:48:33 +0700

    Delay redraws on palette changes

--- st.c
+++ st.c
@@ -1997,7 +1997,7 @@ strhandle(void)
 			else if (xsetcolorname(defaultfg, p))
 				fprintf(stderr, "erresc: invalid foreground color: %s\n", p);
 			else
-				redraw();
+				tfulldirt();
 			return;
 		case 11:
 			if (narg < 2)
@@ -2010,7 +2010,7 @@ strhandle(void)
 			else if (xsetcolorname(defaultbg, p))
 				fprintf(stderr, "erresc: invalid background color: %s\n", p);
 			else
-				redraw();
+				tfulldirt();
 			return;
 		case 12:
 			if (narg < 2)
@@ -2023,7 +2023,7 @@ strhandle(void)
 			else if (xsetcolorname(defaultcs, p))
 				fprintf(stderr, "erresc: invalid cursor color: %s\n", p);
 			else
-				redraw();
+				tfulldirt();
 			return;
 		case 4: /* color set */
 			if (narg < 3)
@@ -2042,7 +2042,7 @@ strhandle(void)
 			} else {
                 		if (j == defaultbg)
                     			xclearwin();
-				redraw();
+				tfulldirt();
 			}
 			return;
 		}
