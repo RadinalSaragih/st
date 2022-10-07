30206349813d31a0f7327fe69ac27179a4583873
Radinal Saragih <rss@radinalsaragih@gmail.com>
2022-10-07 07:14:27 +0700

    added alpha settings to xresources

--- config.def.h
+++ config.def.h
@@ -198,6 +198,7 @@ ResourcePref resources[] = {
 		{ "borderpx",     INTEGER, &borderpx },
 		{ "cwscale",      FLOAT,   &cwscale },
 		{ "chscale",      FLOAT,   &chscale },
+		{ "alpha",	  FLOAT,   &alpha}
 };
 
 /*
