f376cfeff389ed406f4471d37d6701eef075a737
Radinal Saragih <rss@radinalsaragih@gmail.com>
2023-03-10 10:09:26 +0700

    minor tweaks

--- x.c
+++ x.c
@@ -907,8 +907,8 @@ xclearwin(void)
 void
 xhints(void)
 {
-	XClassHint class = {opt_name ? opt_name : "st",
-	                    opt_class ? opt_class : "St"};
+	XClassHint class = {opt_name ? opt_name : termname,
+	                    opt_class ? opt_class : termname};
 	XWMHints wm = {.flags = InputHint, .input = 1};
 	XSizeHints *sizeh;
 
